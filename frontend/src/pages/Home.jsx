1ced5bf6f70eef20e193e92c3b721771c578b234
mohit <mohitgehlot2008@gmail.com>
2025-07-24 12:33:08 +0530

    Add backend and frontend folders

--- frontend/src/pages/Home.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function Home() {
-  return (
-    <div className="text-white text-xl p-6">
-      Welcome to AI Coach Career!
-    </div>
-  );
-}
--- frontend/src/pages/Support.jsx
+++
@@ -1,6 +0,0 @@
-// Support.jsx
-import React from "react";
-
-export default function Support() {
-  return <div className="text-white p-6">Support Page</div>;
-}
